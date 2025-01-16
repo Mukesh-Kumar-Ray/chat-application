01b033ecb4af729e3e08c731974227b9297b8f25
Mukesh-Kumar-Ray <ray.mukesh.2002@gmail.com>
2025-01-17 01:59:57 +0530

    frontend is also redy to upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,14 +7,9 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": {target: "https://web-chat-api-three.vercel.app",
+      "/api": {target: "https://chat-application-api-backend.vercel.app",
         changeOrigin: true,
       },
     },
-  },
-  resolve: {
-    alias: {
-      'react-icons': 'node_modules/react-icons',
-    },
-  },
+  }
 })
