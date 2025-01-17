f72dc15ac46613363e20c53658f56cf735b7e325
Mukesh-Kumar-Ray <ray.mukesh.2002@gmail.com>
2025-01-18 03:57:40 +0530

    done

--- frontend/src/Context/useSendMessage.js
+++ frontend/src/Context/useSendMessage.js
@@ -15,7 +15,7 @@ const UseSendMessage = () => {
       setMessage([...messages, res.data]);
       setLoading(false);
     } catch (error) {
-      console.log("Error in send messages", error);
+      // console.log("Error in send messages", error);
       setLoading(false);
     }
   };
